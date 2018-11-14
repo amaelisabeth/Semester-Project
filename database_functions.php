826b9b14a88c85c00b08223fe0e114c9730cb5a4
vrp56 <vrp56@msstate.edu>
2018-11-14 13:02:58 -0600

    create connection functon for database

---
+++ database_functions.php
@@ -0,0 +1,17 @@
+<?php
+  $hn = 'localhost';  // hostname
+  $db = 'se_project';  // database name 
+  $un = 'root';  // username (root on XAMPP, netID on pluto)
+  $pw = '';  // MySQL password (empty on XAMPP, your MySQL password on pluto)
+  
+  
+  function create_connection() {
+	  $connection = new mysqli($hn, $un, $pw, $db);
+		
+		if ($connection->connect_error) {
+			die($connection->connect_error);
+		}
+  }
+  
+  
+?>
