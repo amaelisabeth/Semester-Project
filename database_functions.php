d674fe15b1547a8a68572c558378907608828922
vrp56 <vrp56@msstate.edu>
2018-11-14 13:09:42 -0600

    actually fixed it

--- database_functions.php
+++ database_functions.php
@@ -1,18 +1,20 @@
 <?php
-  $hn = 'localhost';  // hostname
-  $db = 'se_project';  // database name 
-  $un = 'root';  // username (root on XAMPP, netID on pluto)
-  $pw = '';  // MySQL password (empty on XAMPP, your MySQL password on pluto)
+  
   
   
   function create_connection() {
-	  $connection = new mysqli($hn, $un, $pw, $db);
+	$hn = 'localhost';  // hostname
+	$db = 'se_project';  // database name 
+	$un = 'root';  // username (root on XAMPP, netID on pluto)
+	$pw = '';  // MySQL password (empty on XAMPP, your MySQL password on pluto)
 		
-		if ($connection->connect_error) {
-			die($connection->connect_error);
-		}
-		
-		return $connection;
+	$connection = new mysqli($hn, $un, $pw, $db);
+
+	if ($connection->connect_error) {
+		die($connection->connect_error);
+	}
+
+	return $connection;
   }
   
   
