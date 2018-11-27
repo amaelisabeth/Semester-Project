4035f010fcbf2bb3a02778b11961ccbc358bf417
coltana <coltonhall015@gmail.com>
2018-11-27 13:13:55 -0600

    Forgot Password: Final
    Please check on your browser to ensure accuracy.

--- forgot_password.php
+++ forgot_password.php
@@ -4,21 +4,71 @@
 	<!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	
+
 	<!-- Bootstrap CSS -->
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	
+
   <title>Forgot Password PAGE</title>
-	
+
 </head>
 <body>
 	<h1>Welcome to the Forgot Password Page!</h1>
 
-	<p style="font-style:italic">
-		
-		<a href="login.php" class="btn btn-outline-primary">back</a>
-	</p>
+  <div class="container">
+        <div id="login-row" class="row justify-content-center align-items-center">
 
+      <form method="post" action="forgot_password.php">
+        <div class="form-group">
+          <label for="username" class="text-black">Username:</label>
+          <input type="text" class="form-control" name="username">
+        </div>
+  <div class="form-group">
+          <input type="submit" class="btn btn-primary btn-md" value="Submit">
+        </div>
+      </form>
+    </div>
+    <p style="font-style:italic">
+
+      <a href="login.php" class="btn btn-outline-primary">back</a>
+    </p>
+  </div>
+
+  <?php
+    require_once 'login_functions.php';
+    $connection = new mysqli($hn, $un, $pw, $db);
+
+    if ($connection->connect_error)
+      die($connection->connect_error);
+
+    if(isset($_POST["username"])) {
+      $uname = $_POST["username"];
+      $query = "SELECT passwd AS _msg FROM users
+          WHERE UserID = '" . $uname . "'";
+      $result = $connection->query($query);
+      if (!$result){
+        die($connection->error);
+      }
+    }
+
+
+
+      // If username / password are invalid, print error message
+      // and keep form filled
+      if ($result->num_rows == 0) {
+        echo "<p style=\"color: red\"> The username not found.</p>";
+      }
+      else{
+        //print"Login successful!";
+
+        $pword = $result->fetch_assoc();
+        echo "<p style=\"color: black\"> Your password is '".$pword['_msg']."'";
+        //header('Location: home_page.php');
+        //exit();
+    }
+
+    $connection->close();
+
+  ?>
 
 </body>
 </html>
